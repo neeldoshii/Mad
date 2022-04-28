ee15e17511462c9598ee6816de99789a14a25332
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2022-04-28 22:01:09 +0530

    Learnt Toast, and about Layouts

--- Exp92/app/src/main/java/com/example/exp92/MainActivity.java
+++ Exp92/app/src/main/java/com/example/exp92/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
--- exp_71/app/src/main/java/com/example/exp_71/MainActivity.java
+++ exp_71/app/src/main/java/com/example/exp_71/MainActivity.java
@@ -1,28 +1,26 @@
-package com.example.exp91;
+package com.example.exp_71;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView TV;
-    ToggleButton tgbtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TV = findViewById(R.id.TV);
-        tgbtn = findViewById(R.id.tgbtn);
-
     }
 
-    public void btnClick(View view){
+    public void displaytext(View view) {
 
-        TV.setText(tgbtn.getText());
+        Toast toast=Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT);
+        toast.show();
+
+
 
     }
 }
